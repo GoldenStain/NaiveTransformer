8d6ffd7c88ab2c09d3dd80281faf380636b5adce
goldenstain <xym@MSI>
2025-04-07 22:51:13 +0800

    add uv init files, implemented embedding layer

---
+++ models.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+import math
+
+class InputEmbeddings(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        # Why do we multiply the sqrt of self.d_model here? see note1
+        return self.embedding(x) * math.sqrt(self.d_model)
