6a7e677072e5ed17041fe2d098aec33669815f10
goldenstain <xym@MSI>
2025-04-08 21:19:37 +0800

    add MultiHeadAttentionBlock and detailed annotations

--- models.py
+++ models.py
@@ -3,22 +3,22 @@ import torch
 import math
 
 class InputEmbeddings(nn.Module):
-    def __init__(self, d_model: int, vocab_size: int):
+    def __init__(self, d_model: int, vocab_size: int) -> None:
         super().__init__()
         self.d_model = d_model
         self.vocab_size = vocab_size
         self.embedding = nn.Embedding(vocab_size, d_model)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         # Why do we multiply the sqrt of self.d_model here? see note1
         return self.embedding(x) * math.sqrt(self.d_model)
 
 class PositionalEmbedding(nn.Module):
-    def __init__(self, d_model: int, seq_len: int, dropout: float):
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
         super().__init__()
         self.d_model = d_model
         self.seq_len = seq_len
-        self.dropout = dropout
+        self.dropout = nn.Dropout(dropout)
 
         # Create the positional embedding tensor
         pe = torch.zeros(seq_len, d_model)
@@ -38,29 +38,73 @@ class PositionalEmbedding(nn.Module):
 
         self.register_buffer('pe', pe)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         # x:(batch_size, seq_len, d_model)
         x = x + (self.pe[:, :x.shape[1], :]).requires_grad(False) # Here, the pe has been registered, so it is ok to not add the `requires_grad` part. It is not learned by default
         return self.dropout(x)
 
 class LayerNormalization(nn.Module):
-    def __init__(self, eps: float = 1e-6):
+    def __init__(self, eps: float = 1e-6) -> None:
         super().__init__()
         self.eps = eps
         self.alpha = nn.Parameter(torch.ones(1))
         self.bias = nn.Parameter(torch.zeros(1))
     
-    def forward(self, x:torch.Tensor):
+    def forward(self, x:torch.Tensor) -> None:
         mean = x.mean(dim = -1, keepdim=True)
         std = x.std(dim = -1, keepdim=True)
         return self.alpha * (x - mean) / (std + self.eps) + self.bias
 
 class FeedForwadBlock(nn.Module):
-    def __init__(self, d_model: int, d_ff: int, dropout: float):
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
         super().__init__()
-        self.dropout = dropout
+        self.dropout = nn.Dropout(dropout)
         self.linear1 = nn.Linear(d_model, d_ff)
+
         self.linear2 = nn.Linear(d_ff, d_model)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.linear2(self.dropout(torch.relu(self.linear1(x))))
+    
+class MultiHeadAttentionBlock(nn.Module):
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
+        assert d_model % h == 0, "d_model should be divisible by h"
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        self.dropout = nn.Dropout(dropout)
+
+        self.d_k = d_model // h
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        self.w_o = nn.Linear(d_model, d_model)
+
+    @staticmethod
+    def attention(query: torch.Tensor, key: torch.Tensor, value: torch.Tensor, mask: torch.Tensor, dropout: float) -> tuple[torch.Tensor, torch.Tensor]:
+        d_k = query.shape[-1]
+
+        attention_scores = (query @ key.transpose(-1, -2)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores = attention_scores.masked_fill_(mask==0, -1e9)
+        attention_scores = attention_scores.softmax(dim = -1) # (batch_size, h, seq_len, seq_len)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        
+        return (attention_scores @ value), attention_scores # (batch_size, h, seq_len, d_k)
+
+    def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+        query = self.w_q(q) # type: torch.Tensor
+        key = self.w_k(k) # type: torch.Tensor
+        value = self.w_v(v) # type: torch.Tensor
+
+        # transpose(1, 2) to make sure that each the block is able to see the whole sentence, thought not complete words.
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+
+        x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout) # x is the real output, attention_scores is only for visualization purpose
+
+        # x: (batch_size, h, seq_len, d_k) -> (batch_size, seq_len, d_model)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+        return self.w_o(x)
