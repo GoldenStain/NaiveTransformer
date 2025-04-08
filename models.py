dd981d09b6a7a547d78b4f9314dce5306c7608d3
goldenstain <xym@MSI>
2025-04-08 15:57:20 +0800

    add positional embedding layer

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch
 import math
 
 class InputEmbeddings(nn.Module):
@@ -11,3 +12,34 @@ class InputEmbeddings(nn.Module):
     def forward(self, x):
         # Why do we multiply the sqrt of self.d_model here? see note1
         return self.embedding(x) * math.sqrt(self.d_model)
+
+class PositionalEmbedding(nn.Module):
+    def __init__(self, d_model: int, seq_len: int, dropout: float):
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = dropout
+
+        # Create the positional embedding tensor
+        pe = torch.zeros(seq_len, d_model)
+
+        # the position array (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+
+        # the denominator array
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000) / d_model))
+
+        # If we mutiply position by div_term, then we will trigger the broadcast funciton and we will have a tesor(seq_len, d_model)
+        pe[:, 0::2] = torch.sin(position*div_term)
+        pe[:, 1::2] = torch.cos(position*div_term)
+
+        # add a dimension for batch
+        pe = pe.unsqueeze(0)
+
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        # x:(batch_size, seq_len, d_model)
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad(False) # Here, the pe has been registered, so it is ok to not add the `requires_grad` part. It is not learned by default
+        return self.dropout(x)
+
