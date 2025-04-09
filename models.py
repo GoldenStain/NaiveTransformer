0be39d5b8d7344c347ae7592112de6c40ebc4ddc
goldenstain <xym@MSI>
2025-04-09 17:11:52 +0800

    add Encoder

--- models.py
+++ models.py
@@ -122,6 +122,7 @@ class ResidualConnection(nn.Module):
 
 class EncoderBlock(nn.Module):
     def __init__(self, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwadBlock, dropout: float) -> None:
+        super().__init__()
         self.self_attention_block = self_attention_block
         self.feed_forward_block = feed_forward_block
         self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
@@ -131,4 +132,16 @@ class EncoderBlock(nn.Module):
         # Here, the lambda expression itself will be passed to forward method of ResidualConnection as a callable.
         x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
         x = self.residual_connections[1](x, self.feed_forward_block)
+        return x
+
+class Encoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+    
+    def forward(self, x: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+        for layer in self.layers:
+            x = layer(x, mask)
+        x = self.norm(x)
         return x
