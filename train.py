128d19f9e84453dd2641d7eeb0ac099b26d798e3
GoldenStain <goldenstainxym@gmail.com>
2025-05-01 22:35:31 +0800

    massive update in training

--- train.py
+++ train.py
@@ -15,7 +15,9 @@ from pathlib import Path
 
 from dataset import BilinguaDataset
 from models import build_transformer
-from config import get_weights_file_path
+from config import get_weights_file_path,  latest_weights_file_path
+
+from tqdm import tqdm
 
 
 def get_all_sentences(ds: HFDataset, lang: str) -> Generator[str, None, None]:
@@ -115,3 +117,60 @@ def train_model(config: dict):
     writer = SummaryWriter(config['experiment_name'])
 
     optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'], eps=1e-9)
+
+    # If the user specified a model to preload before training, then load it
+    initial_epoch = 0
+    global_step = 0
+    preload = config['preload']  # type: str
+    model_filename = latest_weights_file_path(config) if preload == 'latest' else (
+        get_weights_file_path(config, preload) if preload else None)
+    if model_filename:
+        print(f"Preloading model: {model_filename}")
+        state = torch.load(model_filename)
+        model.load_state_dict(state['model_state_dict'])
+        initial_epoch = state['epoch'] + 1
+        optimizer.load_state_dict(state['optimizer_state_dict'])
+        global_step = state['global_step']
+    else:
+        print("No model to preload, starting from scratch")
+
+    loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_tgt.token_to_id(
+        '[PAD]'), label_smoothing=0.1).to(device)
+
+    # start training
+    for epoch in range(initial_epoch, config['num_epochs']):
+        torch.cuda.empty_cache()
+        model.train()
+        batch_iterator = tqdm(train_dataloader, desc=f"Processing Epoch {epoch:02d}")
+        for batch in batch_iterator:
+            # clear the grad
+            optimizer.zero_grad(set_to_none=True)
+
+            encoder_input = batch['encoder_input'].to(device) # (B, seq_len)
+            decoder_input = batch['decoder_input'].to(device) # (B, seq_len)
+            encoder_mask = batch['encoder_mask'].to(device) # (B, 1, 1, seq_len)
+            decoder_mask = batch['decoder_mask'].to(device) # (B, 1, seq_len, seq_len)
+
+            # Run tensors through the transformer model
+            encoder_output = model.encode(encoder_input, encoder_mask) # (B, seq_len, d_model)
+            decoder_output = model.decode(decoder_input, encoder_output, encoder_mask, decoder_mask) # (B, seq_len, d_model)
+            proj_output = model.project(decoder_output) # (B, seq_len, vocab_size)
+
+            # Compare the output with the label
+            label = batch['label'].to(device) # (B, seq_len)
+
+            # Compute the loss using a simple cross entropy
+            loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1))
+            batch_iterator.set_postfix({"loss": f"{loss.item():6.3f}"})
+
+            # Log the loss
+            writer.add_scalar('train loss', loss.item(), global_step)
+            writer.flush()
+
+            # Backpropagate the loss
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0) # 梯度裁剪
+            # Update the weights
+            optimizer.step()
+
+            global_step += 1
